6c9096db2ae9514681334734211fc573e517b7d7
Irina <plyvchirina@gmail.com>
2023-01-17 19:25:41 +0300

    step 2

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -1,24 +1,5 @@
 <?php
 
-namespace Differ\Gendiff;
+namespace Differ\Differ;
 
 use Docopt;
-
-function start()
-{
-    $doc = <<<DOC
-    Generate diff
-    
-    Usage:
-      gendiff (-h|--help)
-      gendiff (-v|--version)
-      gendiff [--format <fmt>] <firstFile> <secondFile>
-    
-    Options:
-      -h --help                     Show this screen
-      -v --version                  Show version
-      --format <fmt>                Report format [default: stylish]
-    DOC;
-
-    $args = Docopt::handle($doc, ['version' => 'gendiff v: 0.0.1']);
-}
